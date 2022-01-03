e56d81f9c35abe66b6e47dc3a6ee26e0bf96b723
yzy1015 <zhyyang1015@gmail.com>
2022-01-03 11:36:44 -0500

    add lr scheduler

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
+from tensorflow.keras import regularizers
 
 
 class FineTuneNet(tf.keras.Model):
     def __init__(self, base_net=None, n_class=2, dense_num1=32):
         super(FineTuneNet, self).__init__()
         self.base_net = base_net
-        self.dense1 = tf.keras.layers.Dense(dense_num1)
-        self.dense2 = tf.keras.layers.Dense(n_class)
+        self.dense1 = tf.keras.layers.Dense(dense_num1, kernel_regularizer=regularizers.l2(0.001))
+        self.dense2 = tf.keras.layers.Dense(n_class, kernel_regularizer=regularizers.l2(0.001))
 
     def call(self, x):
         x = self.base_net(x)
--- train.py
+++ train.py
@@ -18,19 +18,24 @@ efficientnet_base = tf.keras.applications.efficientnet.EfficientNetB2(include_to
                                                                       weights='imagenet',
                                                                       input_shape=(224, 224, 3))
 efficientnet_base.trainable = False
+learning_rate = 0.1
 model = FineTuneNet(efficientnet_base, 2)
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9)
 # load data
 train_path, train_label, val_path, val_label = cat_dog_dataset_train_val(config.train_dir, config.num_val_img)
 train_data_generator = ImgDataGen(train_path, train_label, config.batch_size,
-                                preprocess_input=preprocess_input,
-                                transform=config.img_transform)
+                                  preprocess_input=preprocess_input,
+                                  transform=config.img_transform)
 
+train_data_generator.on_epoch_end() # whether to shuffle the data at beginning
 val_data_generator = ImgDataGen(val_path, val_label, config.val_batch_size,
                                 preprocess_input=preprocess_input,
                                 transform=config.img_transform)
 
-'''
+current_best_loss = None
+current_best_acc = None
+val_loss_history = []
+train_loss_history = []
 for epoch in range(config.EP_NUM):
     print("Epoch:", epoch)
     # define metric
@@ -47,41 +52,11 @@ for epoch in range(config.EP_NUM):
         epoch_train_accuracy.update_state(y, model(X, training=False))
 
     train_data_generator.on_epoch_end()
-
-    print('Train loss:', epoch_train_loss_avg.result().numpy(),
-          'Train accuracy:', epoch_train_accuracy.result().numpy())
-
-    # evaluate on val data
-    for i in tqdm(range(len(val_data_generator))):
-        X, y = val_data_generator[i]
-        epoch_val_loss_avg.update_state(classification_loss(model, X, y, training=False))
-        epoch_val_accuracy.update_state(y, model(X, training=False))
-
-    print('Val loss:', epoch_val_loss_avg.result().numpy(),
-          'Val accuracy:', epoch_val_accuracy.result().numpy())
-
-'''
-
-for epoch in range(8):
-    print("Epoch:", epoch)
-    # define metric
-    epoch_train_loss_avg = tf.keras.metrics.Mean()
-    epoch_val_loss_avg = tf.keras.metrics.Mean()
-    epoch_train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
-    epoch_val_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
-    # train for 1 epoch
-    train_data_generator.on_epoch_end()
-    for i in tqdm(range(5)):
-        X, y = train_data_generator[i]
-        loss, grad_vector = gradient(model, classification_loss, X, y)
-        optimizer.apply_gradients(zip(grad_vector, model.trainable_variables))
-        epoch_train_loss_avg.update_state(loss)
-        epoch_train_accuracy.update_state(y, model(X, training=False))
-
-    train_data_generator.on_epoch_end()
-
-    print('Train loss:', epoch_train_loss_avg.result().numpy(),
-          'Train accuracy:', epoch_train_accuracy.result().numpy())
+    train_loss = epoch_train_loss_avg.result().numpy()
+    train_acc = epoch_train_accuracy.result().numpy()
+    train_loss_history.append(train_loss)
+    print('Train loss:', train_loss,
+          'Train accuracy:', train_acc)
 
     # evaluate on val data
     for i in tqdm(range(len(val_data_generator))):
@@ -89,5 +64,28 @@ for epoch in range(8):
         epoch_val_loss_avg.update_state(classification_loss(model, X, y, training=False))
         epoch_val_accuracy.update_state(y, model(X, training=False))
 
-    print('Val loss:', epoch_val_loss_avg.result().numpy(),
-          'Val accuracy:', epoch_val_accuracy.result().numpy())
+    val_loss = epoch_val_loss_avg.result().numpy()
+    val_acc = epoch_val_accuracy.result().numpy()
+    val_loss_history.append(val_loss)
+    print('Val loss:', val_loss,
+          'Val accuracy:', val_acc)
+
+    # loss based lr decay
+    if (len(train_loss_history) > 1) and (train_loss + config.lr_decay_threshold > train_loss_history[-2]):
+        learning_rate = learning_rate / config.lr_decay_ratio
+        optimizer.lr.assign(learning_rate)
+        print('learning rate decay to', optimizer.lr.read_value().numpy())
+
+    if (current_best_loss is None) or (val_acc > current_best_acc) or (val_loss < current_best_loss):
+        current_best_acc = val_acc
+        current_best_loss = val_loss
+        model.save_weights(config.save_format.format(epoch, val_loss, val_acc))
+
+    # optimizer.lr.assign(0.001)
+
+    ''' restart if using adam optimizer
+    if epoch % config.restart_adam_num == 0:
+        for var in optimizer.variables():
+            var.assign(tf.zeros_like(var))
+    '''
+
